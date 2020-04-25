a5a946b8b344453411cf7783e1a23778c0b40912
kimsoowoo <64280594+kimsoowoo@users.noreply.github.com>
2020-04-25 00:51:37 -0400

    wsgi remove

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
-wsgi-file = test.py
+
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
